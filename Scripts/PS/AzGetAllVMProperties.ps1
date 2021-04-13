11cdab9f2186dfbf56468ff9df08cffe782b13f6
Carles González León <36437707+carlesgl81@users.noreply.github.com>
2021-04-13 11:55:14 +0200

    Create AzGetAllVMProperties.ps1
    Modified version of the file found here: https://docs.microsoft.com/en-us/previous-versions/azure/virtual-machines/scripts/virtual-machines-powershell-sample-collect-vm-details

---
+++ Scripts/PS/AzGetAllVMProperties.ps1
@@ -0,0 +1,35 @@
+#Provide the subscription Id where the VMs reside
+$subscriptionId = "xxx-xxx-xxx-xxx-xxx"
+
+#Provide the name of the csv file to be exported
+$reportName = "ManagedServers.csv"
+
+Select-AzSubscription $subscriptionId
+$report = @()
+$vms = Get-AzVM
+$publicIps = Get-AzPublicIpAddress 
+$nics = Get-AzNetworkInterface | ?{ $_.VirtualMachine -NE $null} 
+foreach ($nic in $nics) { 
+    $info = "" | Select VmName, OS, OSVersion, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup 
+    $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id 
+    foreach($publicIp in $publicIps) { 
+        if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
+            $info.PublicIPAddress = $publicIp.ipaddress
+            } 
+        } 
+        $info.OsType = $vm.StorageProfile.OsDisk.OsType 
+        $info.VMName = $vm.Name 
+        $info.OS = $vm.StorageProfile.ImageReference.Sku
+        $info.OSVersion = $vm.StorageProfile.ImageReference.ExactVersion
+        $info.ResourceGroupName = $vm.ResourceGroupName 
+        $info.Region = $vm.Location 
+        $info.VmSize = $vm.HardwareProfile.VmSize
+        $info.VirtualNetwork = $nic.IpConfigurations.subnet.Id.Split("/")[-3] 
+        $info.Subnet = $nic.IpConfigurations.subnet.Id.Split("/")[-1] 
+        $info.PrivateIpAddress = $nic.IpConfigurations.PrivateIpAddress 
+        $info.NicName = $nic.Name 
+        $info.ApplicationSecurityGroup = $nic.IpConfigurations.ApplicationSecurityGroups.Id 
+        $report+=$info 
+    } 
+$report | ft VmName, OS, OSVersion, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup 
+$report | Export-CSV "C:\Temp\$reportName"
